8f698198dcbab8e8d14d844712e84055de0d5acb
shantanu-sharma-kaushal <shantanukaushal9760@gmail.com>
2026-09-01 12:33:16 +0530

    starting

---
+++ greetings.js
@@ -0,0 +1,12 @@
+console.log("hello team , welcome to SIH project");
+team_details ={
+  1: {
+    name: "Shantanu Sharma",
+    year: 4
+  },
+  2: {
+    name: "Parth Garg",
+    year: 4
+  }
+  
+}
--- hi.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    prinitf("hello team , welcome to SIH project");
-    return 0;
-}
